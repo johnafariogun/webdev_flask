661b212a971869f6129a9671be7c926d049811f5
johnafariogun <afariogun.john@yahoo.com>
2023-02-27 17:15:00 +0100

    no

--- git_.sh
+++ git_.sh
@@ -1,6 +1,6 @@
 read -p "please input a commit message  
 " m
-if [$m == 'no']
+if [$m == 'no'];
     then echo "Remember that you have to stay commited and keep your repo up to date"
 else
     git add .
